2d8880663d9e1eb2d7d2d2e315b10e804c6253ae
aparnageorge <aparnageorge94@gmail.com>
2022-06-09 01:37:40 +0100

    Admin Controller - for all user and general tasks

---
+++ controllers/api/adminController.js
@@ -0,0 +1,60 @@
+
+
+// Load Notes model
+const Note = require('../../models/Note');
+// Load User model
+const User = require('../../models/User');
+
+/* from GET api/admin
+@description Get all notes
+*/
+exports.notes = function(req, res) {
+    Note.find()
+      .then(notes => res.json(notes))
+      .catch(err => res.status(404).json({ nonotesfound: 'No Notes found' }));
+  };
+
+/* from GET api/admin/archived
+@description Get all archived notes
+*/
+exports.archivednotes = function(req, res) {
+    Note.find({archived: true })
+      .then(notes => res.json(notes))
+      .catch(err => res.status(404).json({ nonotesfound: 'No Archived Notes found' }));
+  };
+
+/* from GET api/admin/active
+@description Get all active notes/ Get all notes that aren't archived
+*/
+exports.activenotes = function(req, res) {
+    Note.find({archived: false })
+      .then(notes => res.json(notes))
+      .catch(err => res.status(404).json({ nonotesfound: 'No Active Notes found' }));
+  };
+
+/* from GET api/admin/users
+@description Get all users
+*/
+exports.users = function(req, res) {
+    User.find({})
+      .then(users => res.json(users))
+      .catch(err => res.status(404).json({ nousersfound: 'No Users found' }));
+  };
+
+/* from POST api/admin/users
+@description add/save new user
+*/ 
+exports.adduser = function(req, res) {
+  User.create(req.body)
+  .then(user => res.json({ msg: 'User added successfully' }))
+  .catch(err => res.status(400).json({ error: 'Unable to add this user ' + err }));
+  };
+
+/* from DELETE api/admin/users/:id
+@description Delete user by id
+*/ 
+exports.deleteuser = function(req, res) {
+    User.findByIdAndRemove(req.params.id, req.body)
+      .then(user => res.json({ msg: 'User deleted successfully' }))
+      .catch(err => res.status(404).json({ error: 'No such user found ' +err}));
+    };
